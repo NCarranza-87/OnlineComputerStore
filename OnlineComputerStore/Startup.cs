732b121bd32c038cf232e3d4fd573d0046a40a7d
NCarranza-87 <36281270+NCarranza-87@users.noreply.github.com>
2018-10-09 22:43:54 -0700

    Add project files.

---
+++ OnlineComputerStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineComputerStore.Startup))]
+namespace OnlineComputerStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
