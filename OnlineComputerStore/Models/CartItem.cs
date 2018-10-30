4a21bc388825132d5e633b90cf22ac9b27c7589f
NCarranza-87 <36281270+NCarranza-87@users.noreply.github.com>
2018-10-30 16:30:29 -0700

    Added CartItem class

---
+++ OnlineComputerStore/Models/CartItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineComputerStore.Models
+{
+    public class CartItem
+    {
+        [key]
+        public string ItemID { get; set; }
+
+        public string CartID { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int ProductID { get; set; }
+
+        public virtual Product Product { get; set; }
+
+
+    }
+}
