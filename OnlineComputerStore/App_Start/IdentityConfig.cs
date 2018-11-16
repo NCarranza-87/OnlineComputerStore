3e0d15f11b82e2642c5abb70eb257b399858246f
NCarranza-87 <36281270+NCarranza-87@users.noreply.github.com>
2018-11-15 17:54:07 -0800

    Changed Registration and login requirements, No Shopping Cart added to migrations in IdentityMondels

--- OnlineComputerStore/App_Start/IdentityConfig.cs
+++ OnlineComputerStore/App_Start/IdentityConfig.cs
@@ -46,24 +46,24 @@ namespace OnlineComputerStore
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
-                AllowOnlyAlphanumericUserNames = false,
+                AllowOnlyAlphanumericUserNames = true,
                 RequireUniqueEmail = true
             };
 
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = 3,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
             manager.UserLockoutEnabledByDefault = true;
-            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
-            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(3);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 4;
 
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
             // You can write your own provider and plug it in here.
--- OnlineComputerStore/Controllers/HomeController.cs
+++ OnlineComputerStore/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace OnlineComputerStore.Controllers
 {
+    [RequireHttps]
     public class HomeController : Controller
     {
         public ActionResult Index()
